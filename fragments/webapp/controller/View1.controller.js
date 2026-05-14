2d4c49c2f3d792d18ee096e5a6dbe02d18f8b608
Pranivvv <21104031.praniv.warungshe@gmail.com>
2026-05-14 04:28:23 +0000

    created project to learn bout the fragments in sap

--- fragments/webapp/controller/View1.controller.js
+++ fragments/webapp/controller/View1.controller.js
@@ -1,10 +1,35 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment"
+], function (Controller, Fragment) {
+
     "use strict";
 
     return Controller.extend("fragments.fragments.controller.View1", {
-        onInit() {
+
+        onOpenFragment: function () {
+            var oView = this.getView();
+
+            if (!this.pDialog) {
+                this.pDialog = Fragment.load({
+                    id: oView.getId(),
+                    // ✅ Corrected namespace: matches webapp/fragment/Dialog.fragment.xml
+                    name: "fragments.fragments.fragment.Dialog",
+                    controller: this
+                }).then(function (oDialog) {
+                    oView.addDependent(oDialog);
+                    return oDialog;
+                });
+            }
+
+            this.pDialog.then(function (oDialog) {
+                oDialog.open();
+            });
+        },
+
+        onCloseDialog: function () {
+            this.byId("myDialog").close();
         }
+
     });
 });
