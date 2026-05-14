d68c70fcaf0e3bb2f282fdfa24a25b0889210d05
Pranivvv <21104031.praniv.warungshe@gmail.com>
2026-05-14 06:36:50 +0000

    completed the project on localization

---
+++ localization/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["localization/localization/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ localization/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"localization/localization/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
