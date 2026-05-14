172a79c3f1594fbe0f0cbb32ddfe34ae067d721a
Pranivvv <21104031.praniv.warungshe@gmail.com>
2026-05-14 05:54:30 +0000

    completed the project to display data from local json source model

---
+++ odata/webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("odata.odata.controller.View1", {
+        onInit() {
+        }
+    });
+});
---
+++ odata/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"odata/odata/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
