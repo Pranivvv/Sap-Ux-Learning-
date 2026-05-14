63b2ea53b3b22b3120c7bcd32233cba90a65a8a0
Pranivvv <21104031.praniv.warungshe@gmail.com>
2026-05-14 09:00:15 +0000

    created program to add control outside controller

---
+++ custom_control/webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("customcontrol.customcontrol.controller.View1", {
+        onInit() {
+        }
+    });
+});
